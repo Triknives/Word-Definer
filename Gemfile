cba0163c938eada0cf071745c13efa3bba479ee9
Triknives <50305423+Triknives@users.noreply.github.com>
2019-12-20 09:10:27 -0800

    create initial structure for project.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source('https://rubygems.org')
+
+ruby '2.5.1'
+gem('sinatra/reloader')
+gem('sinatra')
+gem('rspec')
+gem('sinatra-contrib')
+gem('capybara')
---
+++ app.rb
@@ -0,0 +1,87 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/album')
+require('./lib/song')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  # @albums = Album.sort
+  erb(:landing_page)
+end
+
+get('/albums') do
+  if params["search"]
+    @albums = Album.search(params[:search])
+  else
+    @albums = Album.sort
+  end
+  erb(:albums)
+end
+
+get('/albums/new') do
+  erb(:new_album)
+end
+
+get('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
+
+
+post('/albums') do
+  values = *params.values
+  album = Album.new(values[0], nil, values[1], values[2], values[3])
+  album.save()
+  @albums = Album.all() # Adding this line will fix the error.
+  erb(:albums)
+end
+
+get('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+patch('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  values = *params.values
+  @album.update(values[1], values[2], values[3], values[4])
+  @albums = Album.all
+  erb(:albums)
+end
+
+delete('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  @albums = Album.all
+  erb(:albums)
+end
+
+# Get the detail for a specific song such as lyrics and songwriters.
+get('/albums/:id/songs/:song_id') do
+  @song = Song.find(params[:song_id].to_i())
+  erb(:songs)
+end
+
+# Post a new song. After the song is added, Sinatra will route to the view for the album the song belongs to.
+post('/albums/:id/songs') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.new(params[:song_name], @album.id, nil)
+  song.save()
+  erb(:album)
+end
+
+# Edit a song and then route back to the album view.
+patch('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.find(params[:song_id].to_i())
+  song.update(params[:name], @album.id)
+  erb(:album)
+end
+
+# Delete a song and then route back to the album view.
+delete('/albums/:id/songs/:song_id') do
+  song = Song.find(params[:song_id].to_i())
+  song.delete
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
