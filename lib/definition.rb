e591cec685b6108056967b5e95128a0871dd70d3
Triknives <50305423+Triknives@users.noreply.github.com>
2020-01-05 15:49:38 -0800

    update functionality

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,52 +1,52 @@
 class Definition
-    attr_reader :id
-    attr_accessor :definitions, :word_id
+  attr_reader :id
+  attr_accessor :definitions, :word_id
 
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(definitions, word_id, id)
+    @definitions = definitions
+    @word_id = word_id
+    @id = id || @@total_rows += 1
+  end
+
+  def ==(definitions_to_compare)
+    (self.definitions() == definitions_to_compare.definitions()) && (self.word_id() == definitions_to_compare.word_id())
+  end
+
+  def self.all
+    @@definitions.values
+  end
+
+  def self.clear
     @@definitions = {}
     @@total_rows = 0
+  end
 
-    def initialize(definitions, word_id, id)
-      @definitions = definitions
-      @word_id = word_id
-      @id = id || @@total_rows += 1
-    end
+  def save
+    @@definitions[self.id] = Definition.new(self.definitions, self.word_id, self.id)
+  end
 
-    def ==(definitions_to_compare)
-      (self.definitions() == definitions_to_compare.definitions()) && (self.word_id() == definitions_to_compare.word_id())
-    end
+  def self.find(id)
+    @@definitions[id]
+  end
 
-    def self.all
-      @@definitions.values
-    end
+  def update(definitions, word_id)
+    self.definitions = definitions
+    self.word_id = word_id
+    @@definitions[self.id] = Definition.new(self.definitions, self.word_id, self.id)
+  end
 
-    def self.clear
-      @@definitions = {}
-      @@total_rows = 0
-    end
+  def delete
+    @@definitions.delete(self.id)
+  end
 
-    def save
-      @@definitions[self.id] = Definition.new(self.definitions, self.word_id, self.id)
-    end
+  def self.clear
+    @@definitions = {}
+  end
 
-    def self.find(id)
-      @@definitions[id]
-    end
-
-    def update(definitions, word_id)
-      self.definitions = definitions
-      self.word_id = word_id
-      @@definitions[self.id] = Definition.new(self.definitions, self.word_id, self.id)
-    end
-
-    def delete
-      @@definitions.delete(self.id)
-    end
-
-    def self.clear
-      @@definitions = {}
-    end
-
-    def self.find_by_word(word_id)
+  def self.find_by_word(word_id)
     words = []
     @@definitions.values.each do |definitions|
       if definitions.word_id == word_id
@@ -56,7 +56,7 @@ class Definition
     words
   end
 
-    def word
-        Dictionairy.find(self.word_id)
-      end
+  def word
+    Dictionairy.find(self.word_id)
   end
+end
